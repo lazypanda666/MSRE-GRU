e682c608fec5ee7d6582099068108f5be8d77dea
lazypanda666 <2484128973@qq.com>
2026-04-14 22:35:43 +0800

    [Feat] 新增实验主函数流程

---
+++ main_our.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+@Description   实验主函数
+@Author        Alex_McAvoy
+@Date          2025-12-02 16:18:22
+"""
+
+import sample.sampling as sample
+import processing.dataProcessing as processing
+
+from const import cfg
+
+if __name__ == "__main__":
+    print("--------------------------------------------")
+    print(f"正在采样 {cfg.DATASET['name']} 数据集")
+    sample.sample()
+    print("--------------------------------------------")
+
+    print(f"正在匿名化")
+    processing.anonymizePcap()
+    print("--------------------------------------------")
+    
+    print(f"正在定长化")
+    processing.truncatePcapPackets()
+    print("--------------------------------------------")
